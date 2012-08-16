163c0f1d3c9bb0b929e3b7fd39b629bf5f9887b4
A. Akpaki <a.akpaki@ingeniousafrica.com>
2012-08-16 21:28:36 +0200

    commit du 16 aout

--- ProjectClient/src/com/ingeniousafrica/android/project/client/activity/Main.java
+++ ProjectClient/src/com/ingeniousafrica/android/project/client/activity/Main.java
@@ -6,28 +6,57 @@ import com.ingeniousafrica.android.project.client.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 public class Main extends Activity implements OnClickListener{
+
+	// Définition des variables globales pour contenir nos controles
+	Button button;
+	EditText nom;
+	EditText prenom;
+	Spinner choix_couleur;
+	Spinner choix_vitesse;
+
+
+
 	/** Called when the activity is first created. */
+	/**Début de l initialisation de notre activity avec la fonction onCreate */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		//recuperation de l instance de notre objet spinner1 avec la methode findViewById
+		//recuperation de l instance de notre objet spinner1 avec la methode findViewById ou creation des accesseurs vers nos controles
 		Spinner s1 =(Spinner)findViewById(R.id.spinner1);
 
-		findViewById(R.id.editText1);
-		findViewById(R.id.editText2);
-		findViewById(R.id.button1).setOnClickListener(this);
+		nom = (EditText)findViewById(R.id.editText1);
+		prenom = (EditText)findViewById(R.id.editText2);
+		button = (Button)findViewById(R.id.button1);
 
+		/** Mettons en place un ecouteur d evenement sur notre controle button */
+		button.setOnClickListener(this);
+
+		// Recuperation de nos données
+		if(this.getIntent().getExtras() != null){
+			String s = this.getIntent().getExtras().getString("Nom");
+			nom.setText(s);
+		}
+		
+		
+		/** Ajoutons un "log" pour voir si cette fonction a été appelée et a quel moment */
+		Log.i("", "onCreate");
+		
+		//Rajout du "setResult"
+		this.setResult(2000);
+		
 		//Utilisation d un adapteur 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, S1couleur);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -68,26 +97,93 @@ public class Main extends Activity implements OnClickListener{
 
 	}
 
-	// Creation de tableau du choix des couleurs
+	/** Creation de tableau du choix des couleurs */
 	private static final String S1couleur [] = { "Blanche", "Noire", "Bleue", "Verte", "Rouge"};
 
 	private static final String s2vitesse [] = {"Automatique", "Mécanique"};
 
+	//Gestion du clique
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.button1:
-			
-			Intent intent = new Intent(Main.this, Fabric.class);
-			startActivity(intent);
-			
+			// Demandons a android de nous pousser une autre activity
+			//Intent intent = new Intent(Main.this, Fabric.class);
+			//startActivity(intent);
+			Intent intent = new Intent(this, Fabric.class);
+			//Envoie de datas a notre sous-activite
+			intent.putExtra("Nom", "Bonjour");
+			this.startActivityForResult(intent, 1000);
+
+
 			break;
 
 		default:
 			break;
 		}
+
+	}
+	
+	
+  // Recuperation des codes des datas de notre sous-activité
+	//avec la methode "onActivityResult"
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Testons si notre code correspond au requestCode renvoye au dessus id 1000
+		if(requestCode == 1000){
+			//si c est ok Renvoyons le resultCode dans le Log
+			Log.i("", ""+resultCode);
+		}
 		
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
-	
+	/** Fonction appelée Lorsque l activité de visible a l user */
+	@Override
+	protected void onStart() {
+		/** Ajoutons un "log" pour voir si cette fonction a été appelée et a quel moment */
+		Log.i("","onStart");
+		super.onStart();
+	}
+
+	/** Fonction appelée lorsque l activité devien reellemet active et en cours d utilisation par l user */
+	@Override
+	protected void onResume() {
+		/** Ajoutons un "log" pour voir si cette fonction a été appelée et a quel moment */
+		Log.i("", "onResume");
+		super.onResume();
+	}
+
+	/** Fonction appelée lorsque l activité est en pause */
+	@Override
+	protected void onPause() {
+		/** Ajoutons un "log" pour voir si cette fonction a été appelée et a quel moment */
+		Log.i("", "onPause");
+		super.onPause();
+	}
+
+	/** Fonction appelée lorsque l activité est stopé par un autre programme du systeme */
+	@Override
+	protected void onStop() {
+		/** Ajoutons un "log" pour voir si cette fonction a été appelée et a quel moment */
+		Log.i("", "onStop");
+		super.onStop();
+	}
+
+
+	/** Fonction appelée a la fin de notre activity */
+	@Override
+	protected void onDestroy() {
+		/** Ajoutons un "log" pour voir si cette fonction a été appelée et a quel moment */
+		Log.i("", "onDestroy");
+		super.onDestroy();
+	}
+
+
+
+
+
+
+
+
 }
 
